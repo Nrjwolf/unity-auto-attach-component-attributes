efbdad496fe0138c3237d15406c8f37b04573c55
Roman Fadeev <roma.fadeev@gmail.com>
2020-06-11 17:12:47 +0800

    Add missing GetComponentInChildrenAttribute implementation

--- AttachComponents/Attributes/AttachAttributes.cs
+++ AttachComponents/Attributes/AttachAttributes.cs
@@ -3,5 +3,17 @@ using System;
 using UnityEngine;
 
 [AttributeUsage(System.AttributeTargets.Field)] public class GetComponentAttribute : PropertyAttribute { }
+
+[AttributeUsage(System.AttributeTargets.Field)]
+public class GetComponentInChildrenAttribute : PropertyAttribute
+{
+    public bool IncludeInactive { get; private set; }
+
+    public GetComponentInChildrenAttribute(bool includeInactive)
+    {
+        IncludeInactive = includeInactive;
+    }
+}
+
 [AttributeUsage(System.AttributeTargets.Field)] public class AddComponentAttribute : PropertyAttribute { }
 [AttributeUsage(System.AttributeTargets.Field)] public class FindObjectOfTypeAttribute : PropertyAttribute { }
