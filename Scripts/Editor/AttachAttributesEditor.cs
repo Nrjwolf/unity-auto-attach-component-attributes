c6137ce7f8478e0b83409c7020e2a1fe5d9fea41
Alexander <nrjwolf@gmail.com>
2020-07-08 19:03:20 +0800

    updated for unity package manager

---
+++ Scripts/Editor/AttachAttributesEditor.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Nrjwolf.Tools.AttachAttributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Nrjwolf.Tools.Editor.AttachAttributes
+{
+    public static class AttachAttributesUtils
+    {
+        private const string k_ContextMenuItemLabel = "CONTEXT/Component/AttachAttributes";
+        private const string k_ToolsMenuItemLabel = "Tools/Nrjwolf/AttachAttributes";
+
+        private const string k_EditorPrefsAttachAttributesGlobal = "IsAttachAttributesActive";
+
+        public static bool IsEnabled
+        {
+            get => EditorPrefs.GetBool(k_EditorPrefsAttachAttributesGlobal, true);
+            set
+            {
+                if (value) EditorPrefs.DeleteKey(k_EditorPrefsAttachAttributesGlobal);
+                else EditorPrefs.SetBool(k_EditorPrefsAttachAttributesGlobal, value); // clear value if it's equals defaultValue
+            }
+        }
+
+        [MenuItem(k_ContextMenuItemLabel)]
+        [MenuItem(k_ToolsMenuItemLabel)]
+        private static void ToggleAction()
+        {
+            IsEnabled = !IsEnabled;
+        }
+
+        [MenuItem(k_ContextMenuItemLabel, true)]
+        [MenuItem(k_ToolsMenuItemLabel, true)]
+        private static bool ToggleActionValidate()
+        {
+            Menu.SetChecked(k_ContextMenuItemLabel, IsEnabled);
+            Menu.SetChecked(k_ToolsMenuItemLabel, IsEnabled);
+            return true;
+        }
+
+        public static string GetPropertyType(this SerializedProperty property)
+        {
+            var type = property.type;
+            var match = Regex.Match(type, @"PPtr<\$(.*?)>");
+            if (match.Success)
+                type = match.Groups[1].Value;
+            return type;
+        }
+
+        public static Type StringToType(this string aClassName) => System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).First(x => x.IsSubclassOf(typeof(Component)) && x.Name == aClassName);
+    }
+
+    /// Base class for Attach Attribute
+    public class AttachAttributePropertyDrawer : PropertyDrawer
+    {
+        private Color m_GUIColorDefault = new Color(.6f, .6f, .6f, 1);
+        private Color m_GUIColorNull = new Color(1f, .5f, .5f, 1);
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // turn off attribute if not active or in Play Mode (imitate as build will works)
+            if (!AttachAttributesUtils.IsEnabled || Application.isPlaying)
+            {
+                property.serializedObject.Update();
+                EditorGUI.PropertyField(position, property, label, true);
+                property.serializedObject.ApplyModifiedProperties();
+                return;
+            }
+
+            bool isPropertyValueNull = property.objectReferenceValue == null;
+
+            // Change GUI color
+            var prevColor = GUI.color;
+            GUI.color = isPropertyValueNull ? m_GUIColorNull : m_GUIColorDefault;
+
+            // Default draw
+            EditorGUI.PropertyField(position, property, label, true);
+
+            // Get property type and GameObject
+            property.serializedObject.Update();
+            if (isPropertyValueNull)
+            {
+                var type = property.GetPropertyType().StringToType();
+                var go = ((MonoBehaviour)(property.serializedObject.targetObject)).gameObject;
+                UpdateProperty(property, go, type);
+            }
+
+            property.serializedObject.ApplyModifiedProperties();
+            GUI.color = prevColor;
+        }
+
+        /// Customize it for each attribute
+        public virtual void UpdateProperty(SerializedProperty property, GameObject go, Type type)
+        {
+            // Do whatever
+            // For example to get component 
+            // property.objectReferenceValue = go.GetComponent(type);
+        }
+    }
+
+    #region Attribute Editors
+
+    /// GetComponent
+    [CustomPropertyDrawer(typeof(GetComponentAttribute))]
+    public class GetComponentAttributeEditor : AttachAttributePropertyDrawer
+    {
+        public override void UpdateProperty(SerializedProperty property, GameObject go, Type type)
+        {
+            property.objectReferenceValue = go.GetComponent(type);
+        }
+    }
+
+    /// GetComponentInChildren
+    [CustomPropertyDrawer(typeof(GetComponentInChildrenAttribute))]
+    public class GetComponentInChildrenAttributeEditor : AttachAttributePropertyDrawer
+    {
+        public override void UpdateProperty(SerializedProperty property, GameObject go, Type type)
+        {
+            GetComponentInChildrenAttribute labelAttribute = (GetComponentInChildrenAttribute)attribute;
+            if (labelAttribute.ChildName == null)
+            {
+                property.objectReferenceValue = go.GetComponentInChildren(type, labelAttribute.IncludeInactive);
+            }
+            else
+            {
+                var child = go.transform.Find(labelAttribute.ChildName);
+                if (child != null)
+                {
+                    property.objectReferenceValue = child.GetComponent(type);
+                }
+            }
+        }
+    }
+
+    /// AddComponent
+    [CustomPropertyDrawer(typeof(AddComponentAttribute))]
+    public class AddComponentAttributeEditor : AttachAttributePropertyDrawer
+    {
+        public override void UpdateProperty(SerializedProperty property, GameObject go, Type type)
+        {
+            property.objectReferenceValue = go.AddComponent(type);
+        }
+    }
+
+    /// FindObjectOfType
+    [CustomPropertyDrawer(typeof(FindObjectOfTypeAttribute))]
+    public class FindObjectOfTypeAttributeEditor : AttachAttributePropertyDrawer
+    {
+        public override void UpdateProperty(SerializedProperty property, GameObject go, Type type)
+        {
+            property.objectReferenceValue = FindObjectsOfTypeByName(property.GetPropertyType());
+        }
+
+        public UnityEngine.Object FindObjectsOfTypeByName(string aClassName)
+        {
+            var assemblies = System.AppDomain.CurrentDomain.GetAssemblies();
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                var types = assemblies[i].GetTypes();
+                for (int n = 0; n < types.Length; n++)
+                {
+                    if (typeof(UnityEngine.Object).IsAssignableFrom(types[n]) && aClassName == types[n].Name)
+                        return UnityEngine.Object.FindObjectOfType(types[n]);
+                }
+            }
+            return new UnityEngine.Object();
+        }
+    }
+    #endregion
+}
---
+++ Scripts/Runtime/AttachAttributes.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace Nrjwolf.Tools.AttachAttributes
+{
+    [AttributeUsage(System.AttributeTargets.Field)] public class GetComponentAttribute : AttachPropertyAttribute { }
+
+    [AttributeUsage(System.AttributeTargets.Field)]
+    public class GetComponentInChildrenAttribute : AttachPropertyAttribute
+    {
+        public bool IncludeInactive { get; private set; }
+        public string ChildName;
+
+        public GetComponentInChildrenAttribute(bool includeInactive = false)
+        {
+            IncludeInactive = includeInactive;
+        }
+
+        public GetComponentInChildrenAttribute(string childName)
+        {
+            ChildName = childName;
+        }
+    }
+
+    [AttributeUsage(System.AttributeTargets.Field)] public class AddComponentAttribute : AttachPropertyAttribute { }
+    [AttributeUsage(System.AttributeTargets.Field)] public class FindObjectOfTypeAttribute : AttachPropertyAttribute { }
+
+    public class AttachPropertyAttribute : PropertyAttribute { }
+}
